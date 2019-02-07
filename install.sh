adebba19b8f9485b7cb7138344591aa4b82bcef9
Shawn Rupp <smrupp16@gmail.com>
2019-02-07 12:05:09 -0700

    Reverting to manual install

--- install.sh
+++ install.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 
 ##############################################################################
-# This script will install scripts for the gitpuller package.
+# This script will install gitpuller to the GOBIN directory.
 # 
 # Required programs:	Go 1.11+
 ##############################################################################
 
-IO="github.com/icwells/go-tools/iotools"
-KP="gopkg.in/alecthomas/kingpin.v2"
-MAIN="gitpuller"
-
-# Get install location
-SYS=$(ls $GOPATH/pkg | head -1)
-PDIR=$GOPATH/pkg/$SYS
-
 echo ""
-echo "Preparing gitpuller package..."
-echo "GOPATH identified as $GOPATH"
-echo ""
+echo "Installing gitpuller package..."
+echo "GOBIN identified as $GOBIN"
 
-# Get dependencies
-for I in $IO $KP ; do
-	if [ ! -e "$PDIR/$I.a" ]; then
-		echo "Installing $I..."
-		go get -u $I
-		echo ""
-	fi
-done
-
-# compOncDB 
-echo "Building main..."
-go build -o bin/$MAIN src/*.go
+go install -i src/*.go
 
 echo "Finished"
 echo ""
